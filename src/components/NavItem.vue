d2026b44d6b18cd94e86a8c428ccf4801b08b632
GeorgeKarpenko <g.r.karpenko@yandex.ru>
2022-08-15 03:13:23 +0300

    nav

---
+++ src/components/NavItem.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { RouterLink, useRoute } from "vue-router";
+import { computed } from "vue";
+
+const props = defineProps<{
+  item: string;
+}>();
+console.log(useRoute());
+
+const current = computed(() => {
+  if (useRoute().name === props.item) {
+    return true;
+  }
+  return false;
+});
+</script>
+
+<template>
+  <router-link
+    :to="{ name: item }"
+    :class="[
+      current
+        ? 'bg-gray-900 text-white'
+        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+    ]"
+    :aria-current="current ? 'page' : undefined"
+    >{{ item }}</router-link
+  >
+</template>
---
+++ src/components/NavLogo.vue
@@ -0,0 +1,7 @@
+<template>
+  <img
+    class="h-8 w-auto"
+    src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=500"
+    alt="Workflow"
+  />
+</template>
